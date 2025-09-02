d57e180c1fea7cd7ea4317e9c17f8abd138e0e47
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-02 23:10:40 +0500

    first commit

---
+++ Patterns.java
@@ -0,0 +1,47 @@
+package patterns;
+
+public class Patterns {
+    public static void main(String[] args) {
+      // *****   
+      // *****   
+      // *****   
+      // *****
+      
+      for (int i = 0; i <4; i++) {
+        for(int j=0;j<5;j++){
+            System.out.print("*");
+        }
+                    System.out.println();
+
+      }
+
+System.out.println("Q2");
+
+//*****
+//*   *
+//*   *
+//*****  
+
+
+//outer loop
+for (int i = 1; i <= 4; i++) {
+//inner loop
+for(int j=1;j<=5;j++){
+
+  if (i==1 || j==1 ||i==4 || j==5 ) {
+    System.out.print("*");
+  } else{
+    System.out.print(" ");
+  }
+}
+
+System.out.println();
+}
+
+
+
+
+    }
+
+    
+}
