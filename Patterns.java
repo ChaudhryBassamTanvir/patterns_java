aca7422e4dedc71c6dbce3dc9149d10a85f203d2
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-02 23:17:00 +0500

    first commit

--- Patterns.java
+++ Patterns.java
@@ -46,8 +46,13 @@ System.out.println("Q3");
 // ***
 // **** 
  for(int i = 1;i<=4;i++){
+  for (int j = 1; j <=i; j++) {
+    System.out.print("*");
+    
+  }
+  System.out.println();
 
-  
+
  }
 
 
