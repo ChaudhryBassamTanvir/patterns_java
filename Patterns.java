d106198f71ad4726b424d4aa4c0a650b365828d0
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-09-03 13:23:27 +0500

    Updated

--- Patterns.java
+++ Patterns.java
@@ -70,6 +70,32 @@ for(int i = 1;i<=4;i++){
 
  }
 
+//Q5
+System.out.println("q5");
+//    *
+//   **
+//  ***
+// ****
+int n=4;
+int m=5;
+for (int i = 1; i <= n; i++) {
+  
+for (int j = 1; j <=n-i; j++) {
+
+System.out.print(" ");
+}
+
+
+for (int j= 1; j <=i; j++) {
+  System.out.print("*");
+  
+}
+
+  
+System.out.println();
+
+}
+
 
 
     }
